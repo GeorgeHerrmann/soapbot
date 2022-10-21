86c63aee75be33870d79476ca49af73ad042e34c
George Herrmann <gvhmann@gmail.com>
2022-10-20 22:55:04 -0400

    Added basic connection to Discord API

---
+++ src/main/java/com/georgster/App.java
@@ -0,0 +1,48 @@
+package com.georgster;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Mono;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String token = "MTAzMjY4NjkwMjAyNjk3MzIyNQ.GgDqeW.j8vKUUyniZkacPA0bd2PaG7L83_DIAY48_XXuo";
+        DiscordClient client = DiscordClient.create(token);
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> {
+          // ReadyEvent example
+          Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
+              Mono.fromRunnable(() -> {
+                final User self = event.getSelf();
+                System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+              }))
+              .then();
+          // MessageCreateEvent example
+          Mono<Void> handlePingCommand = gateway.on(MessageCreateEvent.class, event -> {
+            Message message = event.getMessage();
+            StringBuilder fullMessage = new StringBuilder();
+            for (int i = 0; i < message.getContent().length(); i++) {
+              if (message.getContent().contains("!ping")) {
+                fullMessage.append("pong!");
+                i+=5;
+              }
+            }
+            
+            if (fullMessage.length() > 1) {
+              return message.getChannel().flatMap(channel -> channel.createMessage(fullMessage.toString()));
+            }
+            return Mono.empty();
+          }).then();
+        
+          // combine them!
+          return printOnLogin.and(handlePingCommand);
+        });
+
+        login.block();
+    }
+}
