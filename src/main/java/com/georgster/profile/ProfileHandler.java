ab423462d6c592451501748fba0358385cdaf49a
George Herrmann <gvhmann@gmail.com>
2022-11-15 17:05:24 -0500

    Began setup for ProfileHandler

--- src/main/java/com/georgster/profile/ProfileHandler.java
+++ src/main/java/com/georgster/profile/ProfileHandler.java
@@ -1,5 +1,9 @@
 package com.georgster.profile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
 
 /*
  * Intended to handle Server and User "profiles" inside of SOAP Bot.
@@ -15,4 +19,23 @@ public class ProfileHandler {
      *  Data;
      * }
      */
+
+    static final String PROFILELOCATION = Paths.get(System.getProperty("user.dir"), "src", "main", "java", "com", "georgster", "profile").toString();
+
+    private ProfileHandler() {
+        throw new IllegalStateException("ProfileHandler is not meant to be constructed");
+    }
+
+    public static boolean userProfileExists(String id) {
+        
+    }
+
+    public static void createServerProfile(String id) {
+        File profile = new File(Paths.get(PROFILELOCATION, id).toString());
+        profile.mkdir();
+    }
+
+    public static boolean serverProfileExists(String id) {
+        return Files.exists(Paths.get(PROFILELOCATION, id), LinkOption.NOFOLLOW_LINKS);
+    }
 }
