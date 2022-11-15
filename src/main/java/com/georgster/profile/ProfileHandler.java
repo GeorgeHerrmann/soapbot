e707a21675f1fef08a87c8c3950e542a61423bb5
George Herrmann <gvhmann@gmail.com>
2022-11-15 15:33:35 -0500

    Began profile structure

---
+++ src/main/java/com/georgster/profile/ProfileHandler.java
@@ -0,0 +1,18 @@
+package com.georgster.profile;
+
+
+/*
+ * Intended to handle Server and User "profiles" inside of SOAP Bot.
+ */
+public class ProfileHandler {
+    /*
+     * Profile Structure:
+     * Folder for each server "id"
+     * Inside each folder is folder for users, with a file for each user
+     * The server folder can also hold other information.
+     * The structure for data inside is:
+     * dataID {
+     *  Data;
+     * }
+     */
+}
