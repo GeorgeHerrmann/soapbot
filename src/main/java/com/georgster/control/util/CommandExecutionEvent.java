0ab242339453add951b871df245f34f3fe5671be
BuildTools <unconfigured@null.spigotmc.org>
2023-04-13 21:18:44 -0400

    updated error logging for command execution

--- src/main/java/com/georgster/control/util/CommandExecutionEvent.java
+++ src/main/java/com/georgster/control/util/CommandExecutionEvent.java
@@ -76,7 +76,7 @@ public class CommandExecutionEvent {
                 logger.append("\tArguments found: " + parser.getArguments().toString() + "\n",LogDestination.NONAPI);
                 executeIfPermission(args);
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.append("Caught error: " + e.getMessage() + "\n", LogDestination.SYSTEM, LogDestination.FILE);
                 logger.append("\tInvalid arguments, sending a help message\n", LogDestination.NONAPI);
                 manager.sendText(command.help(), command.getClass().getSimpleName());
             }
