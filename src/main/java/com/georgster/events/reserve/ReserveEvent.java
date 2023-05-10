d2a7451089c18662c467173f204ab356ccda8d80
BuildTools <unconfigured@null.spigotmc.org>
2023-05-10 12:46:39 -0400

    removed log

--- src/main/java/com/georgster/events/reserve/ReserveEvent.java
+++ src/main/java/com/georgster/events/reserve/ReserveEvent.java
@@ -173,7 +173,6 @@ public class ReserveEvent implements SoapEvent {
             if (until < 0 && Math.abs(until) > 60) {
                 until = Math.abs(until);
             }
-            System.out.println(until);
             return until <= 0;
         } else {
             return reserved >= numPeople;
